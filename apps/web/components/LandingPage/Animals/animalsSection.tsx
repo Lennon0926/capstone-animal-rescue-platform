73c66b5b21a1c2650500ec1c97bbdd24d4701f20
fernando2908 <fernando.sierra3@upr.edu>
2026-03-23 21:41:24 -0400

    fix: update link in AnimalsSection to include animal ID for adoption

--- apps/web/components/LandingPage/Animals/animalsSection.tsx
+++ apps/web/components/LandingPage/Animals/animalsSection.tsx
@@ -70,7 +70,7 @@ export default function AnimalsSection() {
                 </p>
 
                 <Link
-                href="/adopt"
+                href="/adopt/{animal.id}"
                 className={styles.learnMore}
               >
                 Conoce Más
